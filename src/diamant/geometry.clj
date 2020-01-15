33e941da5b7c2b1a9718f36e7131bc6b49e8df8e
SpaVec <bott.alix@gmail.com>
2020-01-15 22:31:16 +0100

    Added tile-square and tile-hex
    Added functions to repeat a shape on square and hexagonal grids with
    given spacing and counts for each axis

--- src/diamant/geometry.clj
+++ src/diamant/geometry.clj
@@ -11,3 +11,24 @@
             object
             (model/translate [radius 0 0])
             (model/rotate [0 0 (* angle %1)]))))))
+
+(defn tile-square
+  "Copy object on a square grid of given spacing"
+  [spacing count-x count-y object]
+  (->>
+   (for [x (range count-x) y (range count-y)]
+     (->>
+      object
+      (model/translate [(* x spacing) (* y spacing) 0])))))
+
+(defn tile-hex
+  "Copy object on a hexagonal grid of given spacing"
+  [spacing count-x count-y object]
+  (let [t (/ spacing 1.73205080757)]
+    (->>
+     (for [x (range count-x) y (range count-y)]
+       (model/translate
+        [(+ (* x spacing) (if (odd? y) (/ spacing 2) 0))
+         (* y t 1.5)
+         0]
+        object)))))
