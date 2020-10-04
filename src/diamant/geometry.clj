d63f866241cc8b1795662714ab13711beee4f046
Clément Salaün <clement@air-de-clement.lan>
2020-10-04 23:50:51 +0200

    Add symmetry

--- src/diamant/geometry.clj
+++ src/diamant/geometry.clj
@@ -2,12 +2,22 @@
   (:require [scad-clj.model :as model]))
 
 (defn circleify
-  "Copy object count times around a circle of the given radius"
-  [radius count object]
-  (let [angle (/ (* 2 Math/PI) count)]
+  "Copy object n times around a circle of the given radius"
+  [radius n object]
+  (let [angle (/ (* 2 Math/PI) n)
+        obj (if (vector? object) object [object])]
     (->>
-     (range count)
+     (range n)
      (map #(->>
-            object
+            (get obj (mod %1 (count obj)))
             (model/translate [radius 0 0])
             (model/rotate [0 0 (* angle %1)]))))))
+
+(defn symmetry
+  [block]
+  (->>
+   [-1 1]
+   (map #(->>
+          block
+          (model/scale [%1 1 1])))
+   (apply model/union)))
